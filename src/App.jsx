fbd3b1d4aea51d62500b70d41938262dd0c8ae75
povilas8 <84262840+povilas8@users.noreply.github.com>
2023-08-08 08:40:28 +0300

    react lifecycle events start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/index.css';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Hello Vite</h1>
+    </div>
+  );
+}
+
+export default App;
